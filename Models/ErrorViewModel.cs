aa0ef980055c806a836cab3622ff655eac66cd59
David Strong <dcstrong@dmacc.edu>
2024-04-22 12:02:22 -0500

    Initial project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CampusConnect_Marketplace.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
